c3dc3330ac584aafb5b175454729285d049d724a
E <72756810+alphaemu@users.noreply.github.com>
2020-10-14 12:34:17 -0400

    Create copyToConfigure.java

---
+++ copyToConfigure.java
@@ -0,0 +1,17 @@
+{
+// Configure your Run and Preview buttons here.
+
+// Run button configuration
+  "commands": {
+        "Compile & Run": "javac {{filename}} && java {{filename_no_ext}}",
+        "Compile": "javac {{filename}}",
+        "Run": "java {{filename_no_ext}}"
+  },
+
+// Preview button configuration
+  "preview": {
+        "Project Index (static)": "https://{{domain}}/{{index}}",
+        "Current File (static)": "https://{{domain}}/{{filepath}}",
+        "Box URL": "https://{{domain3000}}/"
+  }
+}
